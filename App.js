6b71057bdc03b4724f712aa8b836950309802256
Ijaaz Omer <ijaaztheemeperor@gmail.com>
2018-11-03 23:06:21 -0700

    Basic React-Native function testing

--- App.js
+++ App.js
@@ -7,24 +7,45 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, Alert} from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor() {
+    super();
+    this.manager = new BleManager();
+
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>Bush did 7/11</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to 2X!</Text>
+        <Text style={styles.instructions}>The healthy habit building app!</Text>
+        {//<Text style={styles.instructions}>{instructions}</Text>
+        }
+
+        <Button
+          onPress = {() =>{
+            Alert.alert('Whoops, this is Nolans job lol');
+          }}
+          title = "Login"
+        />
+
       </View>
+
     );
   }
 }
